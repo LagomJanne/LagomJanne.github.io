8338f99831d0685be4f01786069c4814f11f6fea
LagomJanne <126473450+LagomJanne@users.noreply.github.com>
2023-10-23 15:08:10 +0200

    dog hover klar

--- e.js
+++ e.js
@@ -11,31 +11,4 @@ function burgerC(button) {
         navList.style.display = "block"; // Show the navigation menu
       }
     }
-  }
-  
-  // Check the screen width when the window is resized
-  window.addEventListener('resize', function() {
-    const screenWidth = window.innerWidth;
-    const navList = document.getElementById("nav-list");
-    const burgerButton = document.querySelector(".container");
-  
-    if (screenWidth > 1000) {
-      navList.style.display = "block"; // Show the navigation menu for screen width over 1000px
-      if (burgerButton.classList.contains("change")) {
-        burgerButton.classList.remove("change"); // Untoggle the button state
-      }
-    } else {
-      navList.style.display = "none"; // Hide the navigation menu for screen width below or equal to 1000px
-    }
-  });
-  
-  // Ensure that the navigation menu is displayed by default
-  window.addEventListener('load', function() {
-    const screenWidth = window.innerWidth;
-    const navList = document.getElementById("nav-list");
-  
-    if (screenWidth > 1000) {
-      navList.style.display = "block"; // Show the navigation menu for screen width over 1000px
-    }
-  });
-  
+}
