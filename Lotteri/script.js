af1bf1bee7e72f9ddd1fa14fa53866ea48859d72
LagomJanne <126473450+LagomJanne@users.noreply.github.com>
2023-10-23 13:04:16 +0200

    EE

---
+++ Lotteri/script.js
@@ -0,0 +1,33 @@
+
+
+let vinst_array = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+]
+
+function getvinster (){
+
+    let int_antal_lotter = Number(antal_lotter.value)
+    antal_lotter.value = ""
+
+    const priser_div = document.getElementById("priser_div")
+}
--- e.js
+++ e.js
@@ -1,6 +1,41 @@
-function burgerC(navList) {
-    navList.classList.toggle("change")
-
-}
-
-
+// JavaScript function to handle the burger button click and adjust navigation display
+function burgerC(button) {
+    button.classList.toggle("change"); // Toggle the "change" class for animation
+    const navList = document.getElementById("nav-list"); // Get the navigation menu
+    const screenWidth = window.innerWidth;
+  
+    if (screenWidth <= 1000) {
+      if (navList.style.display === "block") {
+        navList.style.display = "none"; // Hide the navigation menu
+      } else {
+        navList.style.display = "block"; // Show the navigation menu
+      }
+    }
+  }
+  
+  // Check the screen width when the window is resized
+  window.addEventListener('resize', function() {
+    const screenWidth = window.innerWidth;
+    const navList = document.getElementById("nav-list");
+    const burgerButton = document.querySelector(".container");
+  
+    if (screenWidth > 1000) {
+      navList.style.display = "block"; // Show the navigation menu for screen width over 1000px
+      if (burgerButton.classList.contains("change")) {
+        burgerButton.classList.remove("change"); // Untoggle the button state
+      }
+    } else {
+      navList.style.display = "none"; // Hide the navigation menu for screen width below or equal to 1000px
+    }
+  });
+  
+  // Ensure that the navigation menu is displayed by default
+  window.addEventListener('load', function() {
+    const screenWidth = window.innerWidth;
+    const navList = document.getElementById("nav-list");
+  
+    if (screenWidth > 1000) {
+      navList.style.display = "block"; // Show the navigation menu for screen width over 1000px
+    }
+  });
+  
