2e18bdad2e12d4682ca56aa6282fd5b6a5c1818d
LagomJanne <126473450+LagomJanne@users.noreply.github.com>
2023-10-26 13:58:43 +0200

    Snart klar med navbaren

--- e.js
+++ e.js
@@ -1,9 +1,9 @@
-// JavaScript function to handle the burger button click and adjust navigation display
+
 function burgerC(button) {
-    button.classList.toggle("change"); // Toggle the "change" class for animation
-    const navList = document.getElementById("nav-list"); // Get the navigation menu
+    button.classList.toggle("change"); 
+    const navList = document.getElementById("nav-list"); 
     const screenWidth = window.innerWidth;
-  
+
     if (screenWidth <= 1000) {
       if (navList.style.display === "block") {
         navList.style.display = "none"; // Hide the navigation menu
@@ -11,4 +11,13 @@ function burgerC(button) {
         navList.style.display = "block"; // Show the navigation menu
       }
     }
+}
+
+function resizeFunction() {
+  var w = window.innerWidth;
+
+  if (w >= 1000) {
+    document.getElementById('nav-list').style.display ="block";
+    
+  }
 }
